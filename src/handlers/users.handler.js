58b258e3d8550f725a7f1698a103357668711096
kkhtun <khaingkhanthtun@gmail.com>
2022-08-14 18:27:44 +0630

    Removed unused dependencies

--- src/handlers/users.handler.js
+++ src/handlers/users.handler.js
@@ -1,6 +1,1 @@
-const { OAuth2Client } = require("google-auth-library");
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-const moment = require("moment");
-const jwt = require("jsonwebtoken");
-
 module.exports = ({ UsersController }) => ({});
